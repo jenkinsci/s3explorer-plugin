bd827860845fd6fc1f5e45c82a5f4e2a2ad452ba
Nguyen Son <nguyen.son@finc.com>
2019-09-01 17:33:06 +0900

    Save config to filesystem

--- src/main/java/com/finc/s3explorer/S3ExplorerPlugin.java
+++ src/main/java/com/finc/s3explorer/S3ExplorerPlugin.java
@@ -1,11 +1,19 @@
 package com.finc.s3explorer;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import org.kohsuke.stapler.StaplerRequest;
 
 import hudson.Plugin;
+import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 
 public class S3ExplorerPlugin extends Plugin {
@@ -14,16 +22,84 @@ public class S3ExplorerPlugin extends Plugin {
 	private static String AWS_ACCESS_KEY_ID;
 	private static String AWS_SECRET_ACCESS_KEY;
 	private static String BUCKET_NAME;
+	private String configFile;
 	
 	public void start() throws Exception {
 		LOG.info("Starting S3Explorer plugin");
-	}
+        String jenkinsHome = Jenkins.get().getRootDir().getPath();
+        LOG.info("Root dir = " + jenkinsHome);
+        String configPath = jenkinsHome + "/secrets/com.finc.s3explorer";
+        this.configFile = configPath + "/config.properties";
+        
+    	File file = new File(this.configFile);
+        if (file.exists()) {
+    		readConfig();
+        } else {
+        	File dir = file.getParentFile();
+        	if (!dir.exists()) {
+        		if (dir.mkdir()) {
+        			LOG.info("Successfully created config directory: " + dir.getPath());
+        		} else {
+        			LOG.info("Could not create config directory: " + dir.getPath());
+        		}
+        	}
+        }
+     }
 	
 	public void configure(StaplerRequest sr, JSONObject formData) {
 		setAwsRegion(formData.optString("awsRegion"));
 		setAwsAccessKeyId(formData.optString("awsAccessKeyId"));
 		setAwsSecretAccessKey(formData.optString("awsSecretAccessKey"));
 		setBucketName(formData.optString("bucketName"));
+		
+		saveConfig();
+	}
+	
+	private void readConfig() {
+		InputStream input = null;
+		try {
+			input = new FileInputStream(this.configFile);
+			Properties prop = new Properties();
+			prop.load(input);
+			
+			setAwsRegion(prop.getProperty("region"));
+			setAwsAccessKeyId(prop.getProperty("aws_access_key_id"));
+			setAwsSecretAccessKey(prop.getProperty("aws_secret_access_key"));
+			setBucketName(prop.getProperty("bucket"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private void saveConfig() {
+		OutputStream output = null;
+		try {
+			output = new FileOutputStream(this.configFile);
+			Properties prop = new Properties();
+	    	prop.setProperty("region", S3ExplorerPlugin.AWS_REGION);
+	    	prop.setProperty("aws_access_key_id", S3ExplorerPlugin.AWS_ACCESS_KEY_ID);
+	    	prop.setProperty("aws_secret_access_key", S3ExplorerPlugin.AWS_SECRET_ACCESS_KEY);
+	    	prop.setProperty("bucket", S3ExplorerPlugin.BUCKET_NAME);
+			prop.store(output, "Required properties");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (output != null) {
+				try {
+					output.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	public static String getAwsRegion() {
