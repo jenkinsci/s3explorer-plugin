7d02a68a3a565904d4fb8450d046785d44a9b4de
Nguyen Son <nguyen.son@finc.com>
2020-01-17 16:45:19 +0900

    Fix save and load configuration

--- src/main/java/s3explorer/S3ExplorerPlugin.java
+++ src/main/java/s3explorer/S3ExplorerPlugin.java
@@ -1,13 +1,7 @@
 package s3explorer;
 
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Properties;
+import java.io.Serializable;
 import java.util.logging.Logger;
 
 import org.kohsuke.stapler.StaplerRequest;
@@ -17,122 +11,62 @@ import hudson.util.Secret;
 import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 
-public class S3ExplorerPlugin extends Plugin {
-    private final static Logger LOG = Logger.getLogger(S3ExplorerPlugin.class.getName());
-    private static String AWS_REGION = "";
-    private static Secret AWS_ACCESS_KEY_ID = Secret.fromString("");
-    private static Secret AWS_SECRET_ACCESS_KEY = Secret.fromString("");
-    private static String BUCKET_NAME = "";
-    private String configFile;
+public class S3ExplorerPlugin extends Plugin implements Serializable {
+	private static final long serialVersionUID = 26L;
+	private final static Logger LOG = Logger.getLogger(S3ExplorerPlugin.class.getName());
+	private String awsRegion = "";
+	private Secret awsAccessKeyId = Secret.fromString("");
+	private Secret awsSecretAccessKey = Secret.fromString("");
+	private String bucketName = "";
 
-    public void start() throws Exception {
-        LOG.info("Starting S3Explorer plugin");
-        String jenkinsHome = Jenkins.get().getRootDir().getPath();
-        LOG.info("Root dir = " + jenkinsHome);
-        String configPath = jenkinsHome + "/secrets/s3explorer";
-        this.configFile = configPath + "/config.properties";
-        
-        File file = new File(this.configFile);
-        if (file.exists()) {
-            readConfig();
-        } else {
-            File dir = file.getParentFile();
-            if (!dir.exists()) {
-                if (dir.mkdir()) {
-                    LOG.info("Successfully created config directory: " + dir.getPath());
-                } else {
-                    LOG.info("Could not create config directory: " + dir.getPath());
-                }
-            }
-        }
-     }
-    
-    public void configure(StaplerRequest sr, JSONObject formData) {
-        setAwsRegion(formData.optString("awsRegion"));
-        setAwsAccessKeyId(formData.optString("awsAccessKeyId"));
-        setAwsSecretAccessKey(formData.optString("awsSecretAccessKey"));
-        setBucketName(formData.optString("bucketName"));
-        
-        saveConfig();
-    }
-    
-    private void readConfig() {
-        InputStream input = null;
-        try {
-            input = new FileInputStream(this.configFile);
-            Properties prop = new Properties();
-            prop.load(input);
-            
-            setAwsRegion(prop.getProperty("region"));
-            setAwsAccessKeyId(prop.getProperty("aws_access_key_id"));
-            setAwsSecretAccessKey(prop.getProperty("aws_secret_access_key"));
-            setBucketName(prop.getProperty("bucket"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+	public void start() throws Exception {
+		LOG.info("Starting s3explorer.S3ExplorerPlugin");
+		String jenkinsHome = Jenkins.get().getRootDir().getPath();
+		LOG.info("Root dir = " + jenkinsHome);
 
-    private void saveConfig() {
-        OutputStream output = null;
-        try {
-            output = new FileOutputStream(this.configFile);
-            Properties prop = new Properties();
-            prop.setProperty("region", S3ExplorerPlugin.AWS_REGION);
-            prop.setProperty("aws_access_key_id", S3ExplorerPlugin.AWS_ACCESS_KEY_ID.getEncryptedValue());
-            prop.setProperty("aws_secret_access_key", S3ExplorerPlugin.AWS_SECRET_ACCESS_KEY.getEncryptedValue());
-            prop.setProperty("bucket", S3ExplorerPlugin.BUCKET_NAME);
-            prop.store(output, "Required properties");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (output != null) {
-                try {
-                    output.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+		load();
+		LOG.info("Loaded s3explorer.S3ExplorerPlugin's configuration successfully");
+	}
 
-    public static String getAwsRegion() {
-        return S3ExplorerPlugin.AWS_REGION;
-    }
-    
-    public static String getAwsAccessKeyId() {
-        return S3ExplorerPlugin.AWS_ACCESS_KEY_ID.getPlainText();
-    }
-    
-    public static String getAwsSecretAccessKey() {
-        return S3ExplorerPlugin.AWS_SECRET_ACCESS_KEY.getPlainText();
-    }
+	public void configure(StaplerRequest req, JSONObject formData) throws IOException {
+		setAwsRegion(formData.optString("awsRegion"));
+		setAwsAccessKeyId(formData.optString("awsAccessKeyId"));
+		setAwsSecretAccessKey(formData.optString("awsSecretAccessKey"));
+		setBucketName(formData.optString("bucketName"));
 
-    public static String getBucketName() {
-        return S3ExplorerPlugin.BUCKET_NAME;
-    }
-    
-    public static void setAwsRegion(String awsRegion) {
-        S3ExplorerPlugin.AWS_REGION = awsRegion;
-    }
-    
-    public static void setAwsAccessKeyId(String awsAccessKeyId) {
-        S3ExplorerPlugin.AWS_ACCESS_KEY_ID = Secret.fromString(awsAccessKeyId);
-    }
-    
-    public static void setAwsSecretAccessKey(String awsSecretAccessKey) {
-        S3ExplorerPlugin.AWS_SECRET_ACCESS_KEY = Secret.fromString(awsSecretAccessKey);
-    }
+		save();
+		LOG.info("Saved s3explorer.S3ExplorerPlugin's configuration successfully");
+	}
 
-    public static void setBucketName(String bucketName) {
-        S3ExplorerPlugin.BUCKET_NAME = bucketName;
-    }
+	public String getAwsRegion() {
+		return this.awsRegion;
+	}
 
+	public String getAwsAccessKeyId() {
+		return this.awsAccessKeyId.getPlainText();
+	}
+
+	public String getAwsSecretAccessKey() {
+		return this.awsSecretAccessKey.getPlainText();
+	}
+
+	public String getBucketName() {
+		return this.bucketName;
+	}
+
+	public void setAwsRegion(String awsRegion) {
+		this.awsRegion = awsRegion;
+	}
+
+	public void setAwsAccessKeyId(String awsAccessKeyId) {
+		this.awsAccessKeyId = Secret.fromString(awsAccessKeyId);
+	}
+
+	public void setAwsSecretAccessKey(String awsSecretAccessKey) {
+		this.awsSecretAccessKey = Secret.fromString(awsSecretAccessKey);
+	}
+
+	public void setBucketName(String bucketName) {
+		this.bucketName = bucketName;
+	}
 }
